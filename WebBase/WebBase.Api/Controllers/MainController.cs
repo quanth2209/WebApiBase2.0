0511387c2878ab3a5cb0b2051d1f1d2a09fec088
Quan <thquan2209@gmail.com>
2018-11-21 22:03:49 +0700

    Initial

---
+++ WebBase/WebBase.Api/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebBase.Api.Controllers.Base;
+
+namespace WebBase.Api.Controllers
+{
+    [Route("api/main")]
+    public class MainController : BaseController
+    {
+        public MainController()
+        {
+        }
+
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ WebBase/WebBase.Api/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebBase.Api
+{
+    public partial class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Mvc(services);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            Mvc(app);
+        }
+    }
+}
